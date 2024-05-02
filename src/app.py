d250d0f209d585dd8bd9bd9d5fe9899fda01333e
Zach Myers <zachmyers@woosterbrush.com>
2024-05-02 13:47:13 -0400

    add a reaction added handler fetching message

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ import os
 import sys
 import pathlib
 import json
+
 # Use the package we installed
 from slack_bolt import App
 
@@ -37,10 +38,27 @@ def handle_message_event(body, logger):
         channel=channel, name=SLACK_REACTION_EMOJI, timestamp=timestamp
     )
 
+
 @app.event("reaction_added")
 def handle_reaction_added(body, logger):
-    print(json.dumps(body, indent=2))
-    logger.info(json.dumps(body, indent=2))
+    # get message information from event body
+    timestamp = body["event"]["item"]["ts"]
+    channel = body["event"]["item"]["channel"]
+
+    try:
+        # Call the conversations.history method using the WebClient
+        # The client passes the token you included in initialization
+        result = app.conversations_history(
+            channel=channel, inclusive=True, oldest=timestamp, limit=1
+        )
+
+        message = result["messages"][0]
+        # Print message text
+        print(json.dumps(message, indent=2))
+
+    except Exception as e:
+        logger.error(f"Error: {e}")
+
 
 if __name__ == "__main__":
     app.start(port=int(os.environ.get("PORT", 3000)))
